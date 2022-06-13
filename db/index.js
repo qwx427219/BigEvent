3b46a66d36f84576d4cd469e3c2b21881f8e48fb
qfc <1282201286@qq.com>
2022-06-13 10:32:38 +0800

    提交对.gitignore的修改

--- db/index.js
+++ db/index.js
@@ -4,7 +4,7 @@ const { promisify } = require('util')
 const db = mysql.createPool({
     host: '127.0.0.1',
     user: 'root',
-    password: '111111',
+    password: '123456',
     database: 'my_db_01'
 })
 
